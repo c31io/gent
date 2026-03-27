c535002c88c6abb7391d1b441466b34cf9a4008a
Celio Grand <celiogrand@outlook.com>
2026-03-28 00:31:29 +0800

    feat(plugin): add PluginHost trait

--- src-tauri/src/plugins/mod.rs
+++ src-tauri/src/plugins/mod.rs
@@ -1,7 +1,9 @@
 pub mod errors;
 pub mod capabilities;
 pub mod plugin;
+pub mod host;
 
 pub use errors::PluginError;
 pub use capabilities::Capability;
 pub use plugin::{Manifest, Input, Output, Plugin, Context};
+pub use host::PluginHost;
