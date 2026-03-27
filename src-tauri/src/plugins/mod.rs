d8e3561b070638b690fbd9c02df3aeff6096484c
Celio Grand <celiogrand@outlook.com>
2026-03-28 00:32:26 +0800

    feat(plugin): add PluginRegistry

--- src-tauri/src/plugins/mod.rs
+++ src-tauri/src/plugins/mod.rs
@@ -2,8 +2,10 @@ pub mod errors;
 pub mod capabilities;
 pub mod plugin;
 pub mod host;
+pub mod registry;
 
 pub use errors::PluginError;
 pub use capabilities::Capability;
 pub use plugin::{Manifest, Input, Output, Plugin, Context};
 pub use host::PluginHost;
+pub use registry::PluginRegistry;
