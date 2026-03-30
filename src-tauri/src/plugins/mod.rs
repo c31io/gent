3ccda4cb96d5bba42ee806d6ffd323a6df6bb372
Celio Grand <celiogrand@outlook.com>
2026-03-31 03:18:36 +0800

    feat(plugins): export console module

--- src-tauri/src/plugins/mod.rs
+++ src-tauri/src/plugins/mod.rs
@@ -5,6 +5,7 @@ pub mod host;
 pub mod registry;
 pub mod rust_loader;
 pub mod loader;
+pub mod console;
 pub mod commands;
 
 #[cfg(test)] mod integration_test;
@@ -12,6 +13,7 @@ pub mod commands;
 pub use errors::PluginError;
 pub use capabilities::Capability;
 pub use plugin::{Manifest, Input, Output, Plugin, Context};
+pub use console::ConsoleLine;
 pub use host::PluginHost;
 pub use registry::PluginRegistry;
 pub use rust_loader::RustWasmLoader;
