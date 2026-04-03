e375f6fe623ff5772f555a2209bc94459a518027
Celio Grand <celiogrand@outlook.com>
2026-04-03 23:03:25 +0800

    feat(toast): add toast notification component

---
+++ src/components/toast.rs
@@ -0,0 +1,47 @@
+use leptos::prelude::*;
+
+/// Toast notification type
+#[derive(Clone, Debug, PartialEq)]
+pub enum ToastType {
+    Success,
+    Error,
+    Info,
+}
+
+/// A toast message
+#[derive(Clone, Debug)]
+pub struct Toast {
+    pub id: u32,
+    pub message: String,
+    pub toast_type: ToastType,
+}
+
+/// Toast container component
+#[component]
+pub fn ToastContainer(
+    toasts: Signal<Vec<Toast>>,
+    on_dismiss: Callback<u32>,
+) -> impl IntoView {
+    view! {
+        <div class="toast-container">
+            {move || toasts.get().iter().map(|toast| {
+                let class = match toast.toast_type {
+                    ToastType::Success => "toast toast-success",
+                    ToastType::Error => "toast toast-error",
+                    ToastType::Info => "toast toast-info",
+                };
+                view! {
+                    <div class={class}>
+                        <span>{toast.message.clone()}</span>
+                        <button
+                            class="toast-dismiss"
+                            on:click={move |_| on_dismiss.run(toast.id)}
+                        >
+                            "×"
+                        </button>
+                    </div>
+                }
+            }).collect::<Vec<_>>()}
+        </div>
+    }
+}
